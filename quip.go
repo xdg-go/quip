b0ef7a4097d765ab6d85a1b9b337450e346a4031
David Golden <xdg@xdg.me>
2017-12-01 23:01:54 -0500

    Add basic LinesT type

--- quip.go
+++ quip.go
@@ -1,11 +1,45 @@
 package quip
 
-import "io"
+import (
+	"bufio"
+	"io"
+)
 
+// Parser provides quick line, word, character, etc. parsing against an
+// io.Reader.
 type Parser struct {
 	r io.Reader
 }
 
+// LinesT is a variant type representing either a slice of strings or an error
+type LinesT struct {
+	ss  []string
+	err error
+}
+
+// New construct a new quip.Parser from an io.Reader
 func New(r io.Reader) *Parser {
 	return &Parser{r: r}
+}
+
+// Lines returns a LinesT representing either lines of the input or an error.
+func (p Parser) Lines() LinesT {
+	ss := make([]string, 0)
+	scanner := bufio.NewScanner(p.r)
+
+	for scanner.Scan() {
+		ss = append(ss, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		return LinesT{err: err}
+	}
+
+	return LinesT{ss: ss}
+}
+
+// Unbox returns the underlying slice of strings and error.  One or the other
+// will be nil.
+func (lt LinesT) Unbox() ([]string, error) {
+	return lt.ss, lt.err
 }
--- quip_test.go
+++ quip_test.go
@@ -2,11 +2,15 @@ package quip_test
 
 import (
 	"bytes"
+	"strings"
 	"testing"
 
 	"github.com/xdg/quip"
 	"github.com/xdg/testy"
 )
+
+var TestStrings = []string{"Line one", "Line two", "Line three"}
+var TestInput = strings.Join(TestStrings, "\n")
 
 func TestNew(t *testing.T) {
 	is := testy.New(t)
@@ -16,4 +20,17 @@ func TestNew(t *testing.T) {
 	q := quip.New(&b)
 
 	is.NotNil(q)
+}
+
+func TestLines(t *testing.T) {
+	is := testy.New(t)
+	defer func() { t.Logf(is.Done()) }()
+
+	b := bytes.NewBufferString(TestInput)
+	q := quip.New(b)
+
+	ss, err := q.Lines().Unbox()
+	is.Nil(err)
+	is.Equal(ss, TestStrings)
+
 }
